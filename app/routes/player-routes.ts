4a840d14e8f68827f507fefa346ec9c9a0138d71
navtemmt <83179960+navtemmt@users.noreply.github.com>
2025-09-23 02:04:07 +0800

    fix: remove .ts extensions from imports in player-routes.ts for Node compatibilityfix: remove .ts extensions from imports in player-routes.ts for Node compatibilityUpdate player-routes.ts

--- app/routes/player-routes.ts
+++ app/routes/player-routes.ts
@@ -1,10 +1,6 @@
 import * as express from 'express';
-import * as player_controller from '../controllers/player-controller.ts';
-
+import * as player_controller from '../controllers/player-controller';
 const router = express.Router();
-
 router.get('/', player_controller.get);
-
 router.delete('/:name', player_controller.remove);
-
 export default router;
