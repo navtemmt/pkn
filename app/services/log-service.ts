682253eb67cd16793018ea0c14d9093970e41af2
navtemmt <83179960+navtemmt@users.noreply.github.com>
2025-09-23 02:46:59 +0800

    Update log-service.ts

--- app/services/log-service.ts
+++ app/services/log-service.ts
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
-import type { Response } from '../utils/error-handling-utils.ts';
-import { Data, Log } from '../interfaces/log-processing-interfaces.ts';
+import type { Response } from '../utils/error-handling-utils';
+import { Data, Log } from '../interfaces/log-processing-interfaces';
 
 export class LogService {
     private game_id: string;
